0dff486bba2bce4286d990dbc6cd4cd474314818
GHFHF886 <113959195+GHFHF886@users.noreply.github.com>
2022-09-23 11:50:31 +0800

    Update Overwall_Patch.sh

--- Overwall_Patch.sh
+++ Overwall_Patch.sh
@@ -13,14 +13,14 @@ do
 done
 
 # 防火墙规则部分(CDN)
-sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/eFw58nNRXXfTwU4#https://cdn.jsdelivr.net/gh/GHFHF886/fast_list-2/China_IPList#g' ${root}/update
-sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/t8eOh94EJIHTXR6#https://cdn.jsdelivr.net/gh/GHFHF886/fast_list-2/China_IPv6List#g' ${root}/update
-sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/avtPeqDKt645Arm#https://cdn.jsdelivr.net/gh/GHFHF886/fast_list-2/GFW_List#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/eFw58nNRXXfTwU4#https://gitee.com/fsdfsdfsdddddddd/cls/releases/download/vxgdgd/China_IPList#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/t8eOh94EJIHTXR6#https://gitee.com/fsdfsdfsdddddddd/cls/releases/download/vxgdgd/China_IPv6List#g' ${root}/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/avtPeqDKt645Arm#https://gitee.com/fsdfsdfsdddddddd/cls/releases/download/vxgdgd/GFW_List#g' ${root}/update
 
 # 防火墙规则部分(GITHUB)
-sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/eFw58nNRXXfTwU4#https://raw.githubusercontent.com/GHFHF886/fast_list-2/main/China_IPList#g' ${root}/update
-sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/t8eOh94EJIHTXR6#https://raw.githubusercontent.com/GHFHF886/fast_list-2/main/China_IPv6List#g' ${root}/update
-sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/avtPeqDKt645Arm#https://raw.githubusercontent.com/GHFHF886/fast_list-2/main/GFW_List#g' ${root}/update
+sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/eFw58nNRXXfTwU4#https://gitee.com/fsdfsdfsdddddddd/cls/releases/download/vxgdgd/China_IPList#g' ${root}/update
+sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/t8eOh94EJIHTXR6#https://gitee.com/fsdfsdfsdddddddd/cls/releases/download/vxgdgd/China_IPv6List#g' ${root}/update
+sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/avtPeqDKt645Arm#https://gitee.com/fsdfsdfsdddddddd/cls/releases/download/vxgdgd/GFW_List#g' ${root}/update
 
 # 分流域名部分
 sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP#https://cdn.jsdelivr.net/gh/GHFHF886/fast_list-2/Bypass#g' ${root}/preload
